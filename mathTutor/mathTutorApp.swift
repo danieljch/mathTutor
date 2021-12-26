746cf783aed003fe705ca12d81b6b8b9b804914f
Daniel JCH <danielj.callisayah@gmail.com>
2021-12-26 15:46:46 -0400

    Initial Commit

---
+++ mathTutor/mathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathTutorApp.swift
+//  mathTutor
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct mathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
